087c1925615f44dddc1231805bba144acd1d622c
Nitin Verma <phnx4um@gmail.com>
2020-05-03 02:42:12 +0530

    added support for more countries and next button

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 var CORONA_STAT_ENDPOINT = "https://api.covid19api.com/summary"
 
+var countries = [
+    "United Kingdom",
+    "Spain",
+    "Italy",
+    "France",
+    "United States Of America",
+    "Germany",
+    "Russian Federation",
+    "Iran, Islamic Republic Of",
+    "Brazil",
+    "China",
+    "Canada",
+    "Netherlands",
+    "India"
+]
 
-// fetch(CORONA_STAT_ENDPOINT)
-//     .then((response) => response.json())
-//     .then((stats) => {
-//         document.getElementById("totalCases").innerText = stats["Global"]["TotalConfirmed"]
-//         document.getElementById("totalDeaths").innerText = stats["Global"]["TotalDeaths"]
-//         document.getElementById("totalRecovered").innerText = stats["Global"]["TotalRecovered"]
-//     })
+const nextCountryButton = document.getElementById("next")
+nextCountryButton.addEventListener("click", getCountryStats);
 
 async function coronaStats() {
     let response = await fetch(CORONA_STAT_ENDPOINT);
     let stats = await response.json();
+    console.log(response);
     return stats;
 }
 
@@ -19,28 +30,64 @@ function filterValue(obj, key, value) {
     return obj.find(function(v) { return v[key] === value });
 }
 
+function populateCountryUI(countryStats) {
+    document.getElementById("countryCaseCount").innerText = countryStats["TotalConfirmed"]
+    document.getElementById("countryDeathCount").innerText = countryStats["TotalDeaths"]
+    document.getElementById("countryRecCount").innerText = countryStats["TotalRecovered"]
+}
+
+function capitalizeFirstLetter(string) {
+    let words = string.toUpperCase().split(" ")
+    for (var i = 0; i < words.length; i++) {
+        words[i] = words[i].charAt(0) + words[i].slice(1).toLowerCase()
+    }
+    return words.join(" ")
+}
+
+function getCountryStats() {
+    let countryName = document.getElementById("countryName").innerText;
+    console.log(countryName);
+    // get index of the element 
+    let countryIndex = countries.indexOf(capitalizeFirstLetter(countryName))
+    console.log(countryIndex);
+    //  get index for next country 
+    if (countryIndex === (countries.length - 1)) {
+        // its the last element. set index to 1st element => 0
+        countryIndex = 0
+    } else {
+        countryIndex = countryIndex + 1
+    }
+    // get next country
+    let country = countries[countryIndex]
+    console.log(country)
+    document.getElementById("countryName").innerText = country
+    let countryStats = filterValue(countriesStats, "Country", country)
+    console.log(countryStats)
+    populateCountryUI(countryStats)
+}
+
+
 function getData() {
     coronaStats().
     then(coronaStats => {
-        globalStats = coronaStats["Global"];
-        countriesStats = coronaStats["Countries"]
-        date = coronaStats["Date"]
+        let globalStats = coronaStats["Global"];
+        countriesStats = coronaStats["Countries"];
+        let date = coronaStats["Date"];
 
-
+        //  global stats
         document.getElementById("numCases").innerText = globalStats["TotalConfirmed"]
         document.getElementById("numDeaths").innerText = globalStats["TotalDeaths"]
         document.getElementById("numRecovered").innerText = globalStats["TotalRecovered"]
 
-        // get the country value from the storage API
-        var country = "India"
-            // get the stats for the country from countryStats
-        var countryStats = filterValue(countriesStats, "Country", country)
-        console.log(countryStats)
-        document.getElementById("countryCaseCount").innerText = countryStats["TotalConfirmed"]
-        document.getElementById("countryDeathCount").innerText = countryStats["TotalDeaths"]
-        document.getElementById("countryRecCount").innerText = countryStats["TotalRecovered"]
-
+        //date
         document.getElementById("updatetime").innerText = date
+
+        //country stats
+        let country = "India"
+            // get the stats for the country from countryStats
+        let countryStats = filterValue(countriesStats, "Country", country)
+        console.log(countryStats)
+        populateCountryUI(countryStats)
 
     });
 }
