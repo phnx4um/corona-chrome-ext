672888dfe6c443d552c7a1dd39ac6037edf1ac95
Nitin Verma <phnx4um@gmail.com>
2020-05-03 05:25:45 +0530

    error in retrieving data from USA,iran fixed because of 'of'

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ var countries = [
     "Spain",
     "Italy",
     "France",
-    "United States Of America",
+    "United States of America",
     "Germany",
     "Russian Federation",
-    "Iran, Islamic Republic Of",
+    "Iran, Islamic Republic of",
     "Brazil",
     "China",
     "Canada",
@@ -16,10 +16,10 @@ var countries = [
     "India"
 ]
 
-const nextCountryButton = document.getElementById("next")
+const nextCountryButton = document.getElementById("next");
 nextCountryButton.addEventListener("click", getNextCountry);
 
-const prevCountryButton = document.getElementById("prev")
+const prevCountryButton = document.getElementById("prev");
 prevCountryButton.addEventListener("click", getPrevCountry);
 
 // API call
@@ -32,11 +32,17 @@ async function coronaStats() {
 
 // utility functions
 function capitalizeFirstLetter(string) {
-    let words = string.toUpperCase().split(" ")
+    let lowercase_list = ["AND", "OF"];
+    let words = string.toUpperCase().split(" ");
     for (var i = 0; i < words.length; i++) {
-        words[i] = words[i].charAt(0) + words[i].slice(1).toLowerCase()
+        if (lowercase_list.includes(words[i])) {
+            words[i] = words[i].toLowerCase();
+            continue;
+        }
+        words[i] = words[i].charAt(0) + words[i].slice(1).toLowerCase();
     }
-    return words.join(" ")
+    console.log(words.join(" "));
+    return words.join(" ");
 }
 
 function filterValue(obj, key, value) {
@@ -45,18 +51,18 @@ function filterValue(obj, key, value) {
 
 
 function populateCountryUI(countryStats) {
-    document.getElementById("countryCaseCount").innerText = countryStats["TotalConfirmed"]
-    document.getElementById("countryDeathCount").innerText = countryStats["TotalDeaths"]
-    document.getElementById("countryRecCount").innerText = countryStats["TotalRecovered"]
+    document.getElementById("countryCaseCount").innerText = countryStats["TotalConfirmed"];
+    document.getElementById("countryDeathCount").innerText = countryStats["TotalDeaths"];
+    document.getElementById("countryRecCount").innerText = countryStats["TotalRecovered"];
 }
 
 function getCountryStats(countryIndex) {
-    let country = countries[countryIndex]
-    console.log(country)
-    document.getElementById("countryName").innerText = country
-    let countryStats = filterValue(countriesStats, "Country", country)
-    console.log(countryStats)
-    populateCountryUI(countryStats)
+    let country = countries[countryIndex];
+    console.log(country);
+    document.getElementById("countryName").innerText = country;
+    let countryStats = filterValue(countriesStats, "Country", country);
+    console.log(countryStats);
+    populateCountryUI(countryStats);
 }
 
 // callback function for next button
@@ -69,11 +75,11 @@ function getNextCountry() {
     //  get index for next country 
     if (countryIndex === (countries.length - 1)) {
         // its the last element. set index to 1st element => 0
-        countryIndex = 0
+        countryIndex = 0;
     } else {
-        countryIndex = countryIndex + 1
+        countryIndex = countryIndex + 1;
     }
-    getCountryStats(countryIndex)
+    getCountryStats(countryIndex);
 }
 
 // callback function for prev button
@@ -81,16 +87,16 @@ function getPrevCountry() {
     let countryName = document.getElementById("countryName").innerText;
     console.log(countryName);
     // get index of the element 
-    let countryIndex = countries.indexOf(capitalizeFirstLetter(countryName))
+    let countryIndex = countries.indexOf(capitalizeFirstLetter(countryName));
     console.log(countryIndex);
     //  get index for next country 
     if (countryIndex === 0) {
         // its the first element. set index to last element => countries.length - 1
-        countryIndex = countries.length - 1
+        countryIndex = countries.length - 1;
     } else {
-        countryIndex = countryIndex - 1
+        countryIndex = countryIndex - 1;
     }
-    getCountryStats(countryIndex)
+    getCountryStats(countryIndex);
 }
 
 // this is called when popup is opened
@@ -102,19 +108,20 @@ function getData() {
         let date = coronaStats["Date"];
 
         //  global stats
-        document.getElementById("numCases").innerText = globalStats["TotalConfirmed"]
-        document.getElementById("numDeaths").innerText = globalStats["TotalDeaths"]
-        document.getElementById("numRecovered").innerText = globalStats["TotalRecovered"]
+        document.getElementById("numCases").innerText = globalStats["TotalConfirmed"];
+        document.getElementById("numDeaths").innerText = globalStats["TotalDeaths"];
+        document.getElementById("numRecovered").innerText = globalStats["TotalRecovered"];
 
         //date
-        document.getElementById("updatetime").innerText = date
+        document.getElementById("updatetime").innerText = date;
 
         //country stats
-        let country = "India"
-            // get the stats for the country from countryStats
-        let countryStats = filterValue(countriesStats, "Country", country)
-        console.log(countryStats)
-        populateCountryUI(countryStats)
+        // let country = "India";
+        let country = document.getElementById("countryName").innerText;
+        // get the stats for the country from countryStats
+        let countryStats = filterValue(countriesStats, "Country", country);
+        console.log(countryStats);
+        populateCountryUI(countryStats);
 
     });
 }
