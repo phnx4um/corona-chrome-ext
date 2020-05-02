eaed8c3eb6766227e8f3f997afd09db17d966f71
Nitin Verma <phnx4um@gmail.com>
2020-05-02 21:55:27 +0530

    ??

--- script.js
+++ script.js
@@ -12,16 +12,16 @@ var CORONA_STAT_ENDPOINT = "https://api.covid19api.com/summary"
 async function coronaStats() {
     let response = await fetch(CORONA_STAT_ENDPOINT);
     let stats = await response.json();
-    return stats
+    return stats;
 }
 
 function getData() {
     coronaStats().
     then(coronaStats => {
-        globalStats = coronaStats["Global"]
-        console.log(globalStats["TotalConfirmed"])
-        console.log(globalStats["TotalDeaths"])
-        console.log(globalStats["TotalRecovered"])
+        globalStats = coronaStats["Global"];
+        document.getElementById("totalCases").innerText = globalStats["TotalConfirmed"]
+        document.getElementById("totalDeaths").innerText = globalStats["TotalDeaths"]
+        document.getElementById("totalRecovered").innerText = globalStats["TotalRecovered"]
     });
 }
 
